592462dfb48f63f02094e2af10bf13968008972b
Barapps <lafontante1@montclair.edu>
2016-08-18 00:47:26 -0400

    initial commit

--- app/src/main/java/com/example/edwardlafontant/tic_attack_toe/tpChal.java
+++ app/src/main/java/com/example/edwardlafontant/tic_attack_toe/tpChal.java
@@ -1,11 +1,6 @@
 package com.example.edwardlafontant.tic_attack_toe;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -15,7 +10,6 @@ public class tpChal extends Game_board {
     Button player1, player2;
     TextView one, two;
     int p1, p2;
-
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
